2939098dd33cae502ec5e3dae46cd8bccd687d4c
masiDaniel <masidaniel02@gmail.com>
2024-10-01 10:52:42 +0300

    i dont even know where the problem is

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -4,6 +4,21 @@
 from flask import jsonify, abort
 from api.v1.views import app_views
 
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def authorized() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+        - raise a 403 error
+    """
+    abort(403)
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+        - raise a 401 error
+    """
+    abort(401)
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
