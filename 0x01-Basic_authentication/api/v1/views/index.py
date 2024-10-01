6b0d6bfbbf00abb01b8e343c24b39b121887d052
masiDaniel <masidaniel02@gmail.com>
2024-10-01 10:55:57 +0300

    pycodestyle issues

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -4,6 +4,7 @@
 from flask import jsonify, abort
 from api.v1.views import app_views
 
+
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def authorized() -> str:
     """ GET /api/v1/forbidden
@@ -12,6 +13,7 @@ def authorized() -> str:
     """
     abort(403)
 
+
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
@@ -19,6 +21,7 @@ def unauthorized() -> str:
         - raise a 401 error
     """
     abort(401)
+
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
